7e45345eae349a0d3a5252152b6d05d359bf8f9e
Willem van Dorp <176481991+Willem-van-Dorp@users.noreply.github.com>
2026-04-29 21:53:06 +0100

    Changed relevant modIDs

--- src/main/java/me/WVD/minimal_economy/MinimalEconomy.java
+++ src/main/java/me/WVD/minimal_economy/MinimalEconomy.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package me.WVD.minimal_economy;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -29,8 +29,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(MinimalEconomy.MODID)
+public class MinimalEconomy
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -60,7 +60,7 @@ public class ExampleMod
                 output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
             }).build());
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public MinimalEconomy(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
